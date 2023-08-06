d243406ea692a32e0ed4e21acb0729f03b63c4dc
NickMarcha <exstracrazy@gmail.com>
2023-08-06 08:46:16 +0200

    Build Tables for stat page a bit better

--- client/src/pages/YeeVSPepe.tsx
+++ client/src/pages/YeeVSPepe.tsx
@@ -34,83 +34,50 @@ const YeeVSPepe = () => {
       {donations === null && <div className="loading">Loading...</div>}
       {donations !== null && (
         <div className="flex">
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold">Yee</h1>
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">Sponsor</th>
-                  <th className="px-4 py-2">Sum</th>
-                  <th className="px-4 py-2">Count</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Array.from(donations?.yeeList?.entries())
-                  .sort(([keyA, valueA], [keyB, valueB]) => {
-                    return valueB.sum - valueA.sum;
-                  })
-                  .map(([key, value]) => (
-                    <tr key={key}>
-                      <td className="border px-4 py-2">{key}</td>
-                      <td className="border px-4 py-2">{value.sum}</td>
-                      <td className="border px-4 py-2">{value.count}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold">Pepe</h1>
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">Sponsor</th>
-                  <th className="px-4 py-2">Sum</th>
-                  <th className="px-4 py-2">Count</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Array.from(donations?.pepeList?.entries())
-                  .sort(([keyA, valueA], [keyB, valueB]) => {
-                    return valueB.sum - valueA.sum;
-                  })
-                  .map(([key, value]) => (
-                    <tr key={key}>
-                      <td className="border px-4 py-2">{key}</td>
-                      <td className="border px-4 py-2">{value.sum}</td>
-                      <td className="border px-4 py-2">{value.count}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold">Other</h1>
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">Sponsor</th>
-                  <th className="px-4 py-2">Sum</th>
-                  <th className="px-4 py-2">Count</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Array.from(donations?.otherList?.entries())
-                  .sort(([keyA, valueA], [keyB, valueB]) => {
-                    return valueB.sum - valueA.sum;
-                  })
-                  .map(([key, value]) => (
-                    <tr key={key}>
-                      <td className="border px-4 py-2">{key}</td>
-                      <td className="border px-4 py-2">{value.sum}</td>
-                      <td className="border px-4 py-2">{value.count}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
+          <StatsColumn title="Yee" list={donations.yeeList} />
+          <StatsColumn title="Pepe" list={donations.pepeList} />
+          <StatsColumn title="Other" list={donations.otherList} />
         </div>
       )}
+    </div>
+  );
+};
+
+const StatsColumn: React.FC<{
+  title: string;
+  list: Map<string, { sum: number; count: number }>;
+}> = ({ title, list }) => {
+  return (
+    <div className="flex-1 ml-2 mr-2">
+      <h1 className="text-2xl font-bold">{title}</h1>
+      <h2 className="text-xl font-bold">
+        Total:{" $ "}
+        {Math.round(
+          [...list].map(([key, value]) => value.sum).reduce((a, b) => a + b, 0)
+        )}
+      </h2>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Sponsor</th>
+            <th className="px-4 py-2">Sum USD</th>
+            <th className="px-4 py-2">Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(list?.entries())
+            .sort(([keyA, valueA], [keyB, valueB]) => {
+              return valueB.sum - valueA.sum;
+            })
+            .map(([key, value]) => (
+              <tr key={key}>
+                <td className="border px-4 py-2">{key}</td>
+                <td className="border px-4 py-2">{value.sum}</td>
+                <td className="border px-4 py-2">{value.count}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
