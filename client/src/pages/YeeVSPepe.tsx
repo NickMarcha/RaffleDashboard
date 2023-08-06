b5303e952f40c30957267e1dae17f3ca1bb64376
NickMarcha <exstracrazy@gmail.com>
2023-08-06 18:05:52 +0200

    YeeVSPepe table css layout

--- client/src/pages/YeeVSPepe.tsx
+++ client/src/pages/YeeVSPepe.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchYeeAndPepeLists } from "../raffleApi";
+import RareYee from "../assets/images/RareYee.gif";
+import RarePepe from "../assets/images/RarePepe.gif";
 import "./History.css";
 const YeeVSPepe = () => {
   const [donations, setDonations] = useState<null | {
@@ -34,9 +36,36 @@ const YeeVSPepe = () => {
       {donations === null && <div className="loading">Loading...</div>}
       {donations !== null && (
         <div className="flex">
-          <StatsColumn title="Yee" list={donations.yeeList} />
-          <StatsColumn title="Pepe" list={donations.pepeList} />
-          <StatsColumn title="Other" list={donations.otherList} />
+          <StatsColumn
+            header={
+              <div className="ml-10 items-center content-center inline-flex h-40">
+                <h1 className="text-3xl font-bold">Yee</h1>
+                <img className="h-30 object-fill" src={RareYee} alt="RareYee" />
+              </div>
+            }
+            list={donations.yeeList}
+          />
+          <StatsColumn
+            header={
+              <div className="ml-10 items-center content-center inline-flex h-40">
+                <h1 className="text-3xl font-bold">Pepe</h1>
+                <img
+                  className="h-30 object-fill"
+                  src={RarePepe}
+                  alt="RarePepe"
+                />
+              </div>
+            }
+            list={donations.pepeList}
+          />
+          <StatsColumn
+            header={
+              <div className="ml-10 items-center content-center inline-flex h-40">
+                <h1 className="text-3xl font-bold">Other</h1>
+              </div>
+            }
+            list={donations.otherList}
+          />
         </div>
       )}
     </div>
@@ -44,24 +73,37 @@ const YeeVSPepe = () => {
 };
 
 const StatsColumn: React.FC<{
-  title: string;
   list: Map<string, { sum: number; count: number }>;
-}> = ({ title, list }) => {
+  header?: React.ReactNode;
+}> = ({ list, header: header }) => {
   return (
     <div className="flex-1 ml-2 mr-2">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <h2 className="text-xl font-bold">
-        Total:{" $ "}
-        {Math.round(
-          [...list].map(([key, value]) => value.sum).reduce((a, b) => a + b, 0)
-        )}
-      </h2>
-      <table className="table-auto">
+      {header}
+      <div className="flex flex-row justify-between">
+        <h2 className="ml-10 text-xl font-bold">
+          Total:{" $ "}
+          {Math.round(
+            [...list]
+              .map(([key, value]) => value.sum)
+              .reduce((a, b) => a + b, 0)
+          )}
+        </h2>
+
+        <h2 className="mr-10 text-xl font-bold">
+          TotalCount:{" "}
+          {Math.round(
+            [...list]
+              .map(([key, value]) => value.count)
+              .reduce((a, b) => a + b, 0)
+          )}
+        </h2>
+      </div>
+      <table className="table-fixed	">
         <thead>
           <tr>
-            <th className="px-4 py-2">Sponsor</th>
-            <th className="px-4 py-2">Sum USD</th>
-            <th className="px-4 py-2">Count</th>
+            <th className="px-4 py-2 w-40">Sponsor</th>
+            <th className="px-4 py-2 w-30">Sum USD</th>
+            <th className="px-4 py-2 w-20">Count</th>
           </tr>
         </thead>
         <tbody>
@@ -71,8 +113,17 @@ const StatsColumn: React.FC<{
             })
             .map(([key, value]) => (
               <tr key={key}>
-                <td className="border px-4 py-2">{key}</td>
-                <td className="border px-4 py-2">{value.sum}</td>
+                <td className="border px-4 py-2">
+                  {
+                    <p title={key} className=" w-40 truncate">
+                      {key}
+                    </p>
+                  }
+                </td>
+                <td className="border px-4 py-2">
+                  {"$ "}
+                  {Math.round(value.sum * 10) / 10}
+                </td>
                 <td className="border px-4 py-2">{value.count}</td>
               </tr>
             ))}
